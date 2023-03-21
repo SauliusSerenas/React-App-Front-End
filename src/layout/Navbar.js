c2fe0decca743405ba0052163a65f02839825fb7
SauliusSerenas <115420065+SauliusSerenas@users.noreply.github.com>
2023-03-21 18:10:33 +0200

    Base of front-end app.

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,25 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-body-tertiary bg-primary">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/#">
-                        To Do List
-                    </a>
-                    <button className="navbar-toggle" type="button"
-                            data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation">
+                    <Link className="navbar-brand" to="/">
+                        Employees list
+                    </Link>
+                    <button
+                        className="navbar-toggle"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
                         <span className="navbar-toggle-icon"></span>
                     </button>
 
-                    <button className="btn btn-outline-light">Add User</button>
+                    <Link className="btn btn-outline-light" to="/adduser">
+                        Add Employee
+                    </Link>
                 </div>
             </nav>
         </div>
-    )
+    );
 }
